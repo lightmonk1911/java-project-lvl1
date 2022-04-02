19b4cda3cbe278c781a012e3ec923d58d1a7b683
igor.babenkov <igor.babenkov@corp.mail.ru>
2022-04-02 19:37:54 +0300

    feat: ask user name

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,11 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static String askName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        return scanner.next();
+    }
 }
